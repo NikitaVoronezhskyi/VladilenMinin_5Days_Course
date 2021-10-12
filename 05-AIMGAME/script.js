3b904aa96dae81fff169183248232a42ea17c425
RQu <balanar99@outlook.com>
2021-10-12 11:48:08 +0300

    marathonFinish

--- 05-AIMGAME/script.js
+++ 05-AIMGAME/script.js
@@ -3,6 +3,7 @@ const screens = document.querySelectorAll('.screen')
 const timelist = document.querySelector('#time-list')
 const timeEl = document.querySelector('#time')
 const board = document.querySelector('#board')
+const colors = [`black`,'white','blue','grey','yellow',`pink`,`green`]
 
 let time = 0
 let score = 0
@@ -71,10 +72,22 @@ function createRandomCircle(){
     circle.style.height = `${size}px`
     circle.style.top = `${y}px`
     circle.style.left = `${x}px`
+    setColor(circle)
 
     board.append(circle)
 }
 
 function getRandomNumber(min,max){
    return Math.round(Math.random()*(max-min)+min)
+}
+
+function getRandomColor(e) {
+    const index = Math.floor(Math.random()* colors.length)
+    return colors[index]
+    
+}
+
+function setColor(e) {
+    const color = getRandomColor()
+    e.style.background = color
 }
